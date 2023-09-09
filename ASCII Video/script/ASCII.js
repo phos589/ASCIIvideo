af36c2c1e32ae36f33b50abc562c1b9fc881ee24
Stefan C <140889919+phos589@users.noreply.github.com>
2023-09-09 07:28:00 +0200

    update code

--- ASCII Video/script/ASCII.js
+++ ASCII Video/script/ASCII.js
@@ -1,14 +1,19 @@
+const video = document.getElementById('videoInput');
+const canvasWidth = 1280; 
+const canvasHeight = 760;
+const canvas = new OffscreenCanvas(canvasWidth, canvasHeight);
+canvas.willReadFrequently = true;
+const context = canvas.getContext('2d');
 let videoElement;
 let asciiVideoElement;
 let playing = false;
 let fontSize = 1;
 let asciiFrames = [];
 
-// Function to change the ASCII color
 function changeAsciiColor(color) {
     asciiVideoElement.style.color = color;
 }
-// Event listener for the color picker input
+
 colorPicker.addEventListener('input', function() {
 const selectedColor = colorPicker.value;
 changeAsciiColor(selectedColor);
@@ -45,7 +50,7 @@ function playVideo() {
         playing = true;
         document.getElementById('playButton').disabled = true;
         document.getElementById('stopButton').disabled = false;
-        asciiFrames = [];
+
         videoElement.play(); 
 
         videoElement.addEventListener('loadedmetadata', function () {
@@ -54,13 +59,11 @@ function playVideo() {
             const context = canvas.getContext('2d');
             adjustFontSize(videoElement.videoWidth, videoElement.videoHeight);
 
-            // Function to render frames continuously.
             function renderFrame() {
                 if (playing) {
                     context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-                    const asciiFrame = frameToASCII(context.getImageData(0, 0, canvas.width, canvas.height));
+                    const asciiFrame = convertCanvasToAscii(context.getImageData(0, 0, canvas.width, canvas.height));
                     
-                    // Add the ASCII frame to the array
                     asciiFrames.push(asciiFrame);
                     asciiVideoElement.textContent = asciiFrame;
                 } else if (videoElement.paused) {
@@ -79,8 +82,64 @@ function stopVideo() {
     document.getElementById('playButton').disabled = false;
     document.getElementById('stopButton').disabled = true;
 }
-// Function to convert a video frame to ASCII art.
-function frameToASCII(imageData) {
+// Event listener for the file input to select a video file.
+document.getElementById('videoInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (file) {
+        videoElement = document.createElement('video');
+        videoElement.src = URL.createObjectURL(file);
+        videoElement.autoplay = true;
+        videoElement.controls = false;
+        videoElement.style.display = 'none';
+
+        document.body.appendChild(videoElement);
+
+        asciiVideoElement = document.getElementById('asciiVideo');
+        playVideo();
+    }
+});
+
+document.getElementById('playButton').addEventListener('click', playVideo);
+document.getElementById('stopButton').addEventListener('click', stopVideo);
+
+
+const gif = new GIF({
+    workers: 2,
+    quality: 10,
+});
+
+canvas.width = video.width;
+canvas.height = video.height;
+
+// Event listener for when the video is loaded
+video.addEventListener('loadeddata', () => {
+    const duration = video.duration;
+    const frameRate = 10; 
+    const numFrames = Math.floor(duration * frameRate);
+
+    // Function to capture frames and convert to ASCII
+    function captureFrame(frameNumber) {
+        const time = (frameNumber / frameRate);
+        video.currentTime = time;
+
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const asciiFrame = convertCanvasToAscii(ctx.getImageData(0, 0, canvas.width, canvas.height));
+
+        asciiFrames.push(asciiFrame);
+
+        if (frameNumber < numFrames) {
+
+            captureFrame(frameNumber + 1);
+        } else {
+            console.log(asciiFrames);
+        }
+    }
+    captureFrame(0);
+});
+
+// Function to convert a canvas to ASCII (implement this part)
+function convertCanvasToAscii(imageData) {
     const { data, width, height } = imageData;
     const asciiCharacters = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ','];
 
@@ -105,120 +164,46 @@ function frameToASCII(imageData) {
 
     return asciiFrame;
 }
-// Event listener for the file input to select a video file.
-document.getElementById('videoInput').addEventListener('change', function (event) {
-    const file = event.target.files[0];
-    if (file) {
-        videoElement = document.createElement('video');
-        videoElement.src = URL.createObjectURL(file);
-        videoElement.autoplay = true;
-        videoElement.controls = false;
-        videoElement.style.display = 'none';
-
-        document.body.appendChild(videoElement);
-
-        asciiVideoElement = document.getElementById('asciiVideo');
-        playVideo();
-    }
-});
-
-document.getElementById('playButton').addEventListener('click', playVideo);
-document.getElementById('stopButton').addEventListener('click', stopVideo);
 
 
-function downloadAsciiGif(canvas) {
+document.getElementById('downloadButton').addEventListener('click', generateGIF);
+
+function generateGIF() {
     if (asciiFrames.length > 0) {
         const gif = new GIF({
             workers: 2,
-            quality: 10
+            quality: 10,
+            width: videoElement.videoWidth,
+            height: videoElement.videoHeight,
         });
 
-        const dummyCanvas = document.createElement('canvas');
-        const dummyContext = dummyCanvas.getContext('2d');
-
-        // Assuming canvas dimensions match the dimensions of your ASCII frames
-        dummyCanvas.width = canvas.width;
-        dummyCanvas.height = canvas.height;
-
-        // Add each frame to the GIF
+        // Loop through the ASCII frames and convert them back to image frames
         for (let i = 0; i < asciiFrames.length; i++) {
-            // Check if the dimensions of the current frame match the canvas dimensions
-            if (asciiFrames[i].length !== canvas.height || asciiFrames[i][0].length !== canvas.width) {
-                console.error(`Frame ${i} dimensions do not match the canvas dimensions.`);
-                continue;
-            }
-
-            const frameImageData = dummyContext.createImageData(dummyCanvas.width, dummyCanvas.height);
-            // Convert the ASCII frame back to image data
-            frameImageData.data.set(asciiToImageData(asciiFrames[i]));
-
-            // Draw the image data on the dummy canvas
-            dummyContext.putImageData(frameImageData, 0, 0);
-
-            // Convert the dummy canvas to an image element
-            const frameImage = new Image();
-            frameImage.src = dummyCanvas.toDataURL("image/png");
-
-            gif.addFrame(frameImage, { delay: 100 });
+            const asciiFrame = asciiFrames[i];
+            const imageFrame = document.createElement('canvas');
+            imageFrame.width = videoElement.videoWidth;
+            imageFrame.height = videoElement.videoHeight;
+            const context = imageFrame.getContext('2d');
+            context.font = fontSize + 'px monospace';
+            context.fillStyle = 'black';
+            context.fillRect(0, 0, imageFrame.width, imageFrame.height);
+            context.fillStyle = 'white';
+        
+            const xCoordinate = 10; 
+            const yCoordinate = 20; 
+        
+            context.fillText(asciiFrame, xCoordinate, yCoordinate);
+            gif.addFrame(imageFrame, { copy: true, delay: 100 }); 
         }
-
-        // Create the GIF and trigger download
-        gif.on('finished', function (blob) {
-            const a = document.createElement('a');
-            a.href = URL.createObjectURL(blob);
-            a.download = 'ascii_video.gif';
-            a.style.display = 'none';
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-        });
 
         gif.render();
+
+        gif.on('finished', function(blob) {
+            const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(blob);
+            downloadLink.download = 'ascii_video.gif';
+            downloadLink.textContent = 'Download ASCII GIF';
+            document.body.appendChild(downloadLink);
+        });
     }
 }
-
-function asciiToImageData(asciiFrame) {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-
-    // Assuming each character in the ASCII frame represents a pixel, set canvas dimensions accordingly
-    canvas.width = asciiFrame[0].length;
-    canvas.height = asciiFrame.length;
-
-    // Get the size of the canvas
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
-
-    // Iterate through the ASCII frame and set pixel values
-    for (let y = 0; y < canvasHeight; y++) {
-        for (let x = 0; x < canvasWidth; x++) {
-            // Check if the current coordinates are within the bounds of the ASCII frame
-            if (y < asciiFrame.length && x < asciiFrame[y].length) {
-                const pixelValue = asciiFrame[y][x]; // Replace this with your logic to convert ASCII to pixel values
-
-                // Ensure pixelValue is a valid color or grayscale value
-                if (isValidPixelValue(pixelValue)) {
-                    context.fillStyle = pixelValue;
-                    context.fillRect(x, y, 1, 1);
-                }
-            }
-        }
-    }
-
-    // Get the image data from the canvas
-    const imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-    return imageData.data;
-}
-function isValidPixelValue(pixelValue) {
-    // Regular expression pattern for valid CSS colors
-    const colorPattern = /^#([0-9a-fA-F]{3}){1,2}$|^rgb\(\d{1,3},\s?\d{1,3},\s?\d{1,3}\)$/i;
-
-    // Regular expression pattern for valid grayscale values (0-255)
-    const grayscalePattern = /^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
-
-    // Check if pixelValue matches either color or grayscale pattern
-    return colorPattern.test(pixelValue) || grayscalePattern.test(pixelValue);
-}
-
-// Event listener for the download button
-document.getElementById('downloadButton').addEventListener('click', downloadAsciiGif);
